f175b73c50f472007332957244ed06a9031f4f24
Jose Rivera <jluis.rivera015@gmail.com>
2025-09-12 18:28:12 -0300

    cooking-pizza

---
+++ assets/js/01-cooking-pizza.js
@@ -0,0 +1,26 @@
+// Crea una constante PREPARATION_MINUTES_PER_INGREDIENT con valor 3.
+
+import { removeAllListeners } from "process";
+
+// Crea una constante EXPECTED_MINUTES_IN_OVEN con valor 25.
+
+// Escribe una función remainingMinutesInOven(actualMinutesInOven) que devuelva cuántos minutos faltan en el horno.
+
+// Escribe una función preparationTimeInMinutes(numberOfIngredients) que devuelva el tiempo de preparación en base a los ingredientes.
+
+// Escribe una función totalTimeInMinutes(numberOfIngredients, actualMinutesInOven) que devuelva el tiempo total: preparación + horno ya transcurrido.
+
+export const PREPARATION_MINUTES_PER_INGREDIENT = 3;
+export const EXPECTED_MINUTES_IN_OVEN = 25;
+
+export function remainingMinutesInOven(actualMinutesInOven) {
+  return EXPECTED_MINUTES_IN_OVEN - actualMinutesInOven;
+}
+
+export function preparationTimeInMinutes(numberOfIngredients) {
+  return numberOfIngredients * PREPARATION_MINUTES_PER_INGREDIENT;
+}
+
+export function totalTimeInMinutes(numberOfIngredients, actualMinutesInOven) {
+  return preparationTimeInMinutes(numberOfIngredients) + actualMinutesInOven;
+}
---
+++ assets/js/index.js
@@ -0,0 +1,49 @@
+// precio = 400000;
+
+// precioSpan = document.querySelector(".precio-inicial");
+// precioSpan.innerHTML = precio;
+
+// let incrementar = document.getElementById("incrementar");
+// let decrementar = document.getElementById("decrementar");
+// let cantidadSpan = document.querySelector(".cantidad");
+
+// let cantidad = 0;
+
+// incrementar.onclick = function () {
+//   cantidad = cantidad + 1;
+//   cantidadSpan.innerHTML = cantidad;
+// };
+// decrementar.onclick = function () {
+//   if (cantidad > 0) {
+//     cantidad = cantidad - 1;
+//     cantidadSpan.innerHTML = cantidad;
+//   }
+// };
+
+// let valorTotal = document.querySelector(".valor-total");
+// setInterval(function () {
+//   valorTotal.innerHTML = precio * cantidad;
+// }, 100);
+
+let imagen = document.querySelector("img");
+imagen.addEventListener("click", () => {
+  if (imagen.style.border == "") {
+    imagen.style.border = "3px solid red";
+  } else {
+    imagen.style.border = "";
+  }
+});
+
+
+// const img = document.getElementById("foto");
+// if (!img) throw new Error("No se encontró la imagen #foto");
+
+// const toggleBorde = () => img.classList.toggle("borde-rojo");
+
+// img.addEventListener("click", toggleBorde);
+// img.addEventListener("keydown", (e) => {
+//   if (e.key === "Enter" || e.key === " ") {
+//     e.preventDefault();
+//     toggleBorde();
+//   }
+// });
